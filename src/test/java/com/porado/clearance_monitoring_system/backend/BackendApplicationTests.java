9c4dd0239c4d79c23783a29c78572cd566faf102
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-05 10:26:47 +0800

    Initial Commit

---
+++ src/test/java/com/porado/clearance_monitoring_system/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porado.clearance_monitoring_system.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
