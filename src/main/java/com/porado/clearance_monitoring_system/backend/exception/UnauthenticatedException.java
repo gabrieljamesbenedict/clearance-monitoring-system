3660373648c0cb69da25d7c53008201104366423
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-26 10:26:57 +0800

    feat: add custom exception for authentication issues

---
+++ src/main/java/com/porado/clearance_monitoring_system/backend/exception/UnauthenticatedException.java
@@ -0,0 +1,7 @@
+package com.porado.clearance_monitoring_system.backend.exception;
+
+public class UnauthenticatedException extends RuntimeException {
+    public UnauthenticatedException() {
+        super();
+    }
+}
