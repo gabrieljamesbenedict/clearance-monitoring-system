6e36ebe1e1349845e520e7adc402e32498f4b615
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-26 09:54:16 +0800

    feat: add subclasses for user (i.e., Student and Employee)

---
+++ src/main/java/com/porado/clearance_monitoring_system/backend/model/Employee.java
@@ -0,0 +1,24 @@
+package com.porado.clearance_monitoring_system.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    private Long id;
+
+    @OneToOne(optional = false)
+    @MapsId
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String employeeNumber;
+}
---
+++ src/main/java/com/porado/clearance_monitoring_system/backend/model/Program.java
@@ -0,0 +1,24 @@
+package com.porado.clearance_monitoring_system.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Program {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long programId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+}
