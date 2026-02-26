8b47fa0349a596eaecbdfd68d4e8adc6d24d0eee
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-26 09:53:50 +0800

    feat: add user classes

---
+++ src/main/java/com/porado/clearance_monitoring_system/backend/model/User.java
@@ -0,0 +1,41 @@
+package com.porado.clearance_monitoring_system.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.porado.clearance_monitoring_system.backend.util.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String middlename;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
---
+++ src/main/java/com/porado/clearance_monitoring_system/backend/util/Role.java
@@ -0,0 +1,6 @@
+package com.porado.clearance_monitoring_system.backend.util;
+
+public enum Role {
+    ROLE_STUDENT,
+    ROLE_EMPLOYEE
+}
