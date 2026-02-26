40020aadff974f1b93633508837ae76563cd2488
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-26 11:51:18 +0800

    fix: change default table names
    Specifically most important is changing User entity table name to "users", since the previous "user" is also a Postgres reserved keyword.

--- src/main/java/com/porado/clearance_monitoring_system/backend/model/Program.java
+++ src/main/java/com/porado/clearance_monitoring_system/backend/model/Program.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Entity
+@Table(name = "programs")
 @Getter
 @Setter
 @NoArgsConstructor
--- src/main/java/com/porado/clearance_monitoring_system/backend/model/Student.java
+++ src/main/java/com/porado/clearance_monitoring_system/backend/model/Student.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Entity
+@Table(name = "students")
 @Getter
 @Setter
 @NoArgsConstructor
