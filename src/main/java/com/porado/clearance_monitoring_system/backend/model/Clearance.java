41c71c810d7cf688f26b692b21ad19a6a2c11c0f
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-05 04:04:27 +0800

    feat: add Clearance model

---
+++ src/main/java/com/porado/clearance_monitoring_system/backend/model/Clearance.java
@@ -0,0 +1,44 @@
+package com.porado.clearance_monitoring_system.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "clearances")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Clearance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clearanceId;
+
+    @Column(nullable = false)
+    private String purpose;
+
+
+    @Column(nullable = false)
+    private String academicYear;
+
+
+    @Column(nullable = false)
+    private String semester;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = true)
+    private Instant deletedAt;
+
+}
